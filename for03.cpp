fb2769040593be48fae4d29222181797458c779c
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-01 19:03:20 +0700

    https://lqdoj.edu.vn/problem/for03

---
+++ for03.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++)
+        if (n%i==0) cout<<i<<" ";
+}
