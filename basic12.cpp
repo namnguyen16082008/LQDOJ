6cb9451b8c0ba018bd33664bb879bf7ce81fda85
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-01 18:53:53 +0700

    https://lqdoj.edu.vn/problem/basic12

---
+++ basic12.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+double n;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin>>n;
+	cout<<fixed<<setprecision(1)<<n*2*3.14<<endl<<n*n*3.14;
+}
