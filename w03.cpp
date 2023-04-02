1540009d1adbf8b9e5af77531b8a9c2369314c18
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-02 11:48:56 +0700

    https://lqdoj.edu.vn/problem/w03

---
+++ w03.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n,d=0;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++){
+        for (ll j=1;j<=n;j++)
+            cout<<j+d<<" ";
+        d++;
+        cout<<endl;
+    }
+}
