348d1b0f7d29d60b7f98d6d7cd58977f41ddade2
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-02 22:00:32 +0700

    https://lqdoj.edu.vn/problem/aplusb

---
+++ aplusb.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll t,a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while (t--){
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+}
