979b7e96fe0e51940d0dee74d63d5bd0a44d58c7
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-21 20:06:48 +0700

    https://lqdoj.edu.vn/problem/py03

---
+++ py03.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<"     *     "<<endl;
+    cout<<"    * *    "<<endl;
+    cout<<"   *   *   "<<endl;
+    cout<<"  *     *  "<<endl;
+    cout<<" *       * "<<endl;
+    cout<<"***********"<<endl;
+    cout<<"     *     "<<endl;
+    cout<<"     *     "<<endl;
+    cout<<"     *     "<<endl;
+}
