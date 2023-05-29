cde06cbe27fb12657d91b963960e73b9a3680c55
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-30 00:28:09 +0700

    https://lqdoj.edu.vn/problem/py05

---
+++ py05.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<386<<endl<<300<<endl<<14749<<endl<<"7.976744186046512"<<endl<<7<<endl<<42;
+}
