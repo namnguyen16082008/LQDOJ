fbc86d8a2af71de5bdd40da03bb693e4cc875414
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-01 21:41:19 +0700

    https://lqdoj.edu.vn/problem/math12

---
+++ math12.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll x,y;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>x>>y;
+    for (ll i=0;i<=8;i+=2) cout<<x<<y<<i<<" ";
+}
