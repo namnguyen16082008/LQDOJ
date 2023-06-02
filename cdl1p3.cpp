0264aa47611f9b1ed4e9aeb1cbab81e564892629
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-02 19:59:19 +0700

    https://lqdoj.edu.vn/problem/cdl1p3

---
+++ cdl1p3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+double n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    cout<<fixed<<setprecision(2)<<n*2.205;
+}
