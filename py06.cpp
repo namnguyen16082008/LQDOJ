993d087c339c9885e80a296c1b6f28448fa1cb99
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-02 20:01:37 +0700

    https://lqdoj.edu.vn/problem/py06

---
+++ py06.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<"DT = 49.92"<<endl<<"CV = 28.4";
+}
