2c80ef9c4d8e2b439f6d728de8e86f496a6fb29e
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-07 23:19:25 +0700

    https://lqdoj.edu.vn/problem/cses1645

--- cses1645.cpp
+++ cses1645.cpp
@@ -11,12 +11,9 @@ int main(){
     cin>>n;
     for (ll i=1;i<=n;i++){
         cin>>a[i];
-        if (i==1) x[i]=0;
-        else{
-            ll o=i-1;
-            while (a[o]>=a[i]) o=x[o];
-            x[i]=o;
-        }
+        ll o=i-1;
+        while (a[o]>=a[i]) o=x[o];
+        x[i]=o;
         cout<<x[i]<<" ";
     }
 }
