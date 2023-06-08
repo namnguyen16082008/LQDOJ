323d7a7e039f0d556668ae74f905c2b443d5c415
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-08 14:00:07 +0700

    https://lqdoj.edu.vn/problem/cdl2p3

---
+++ cdl2p3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    (a>0 && b>0 && c>0) ? cout<<"Ca ba so deu duong" : cout<<"It nhat mot so am";
+}
