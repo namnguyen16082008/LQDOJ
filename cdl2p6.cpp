2c9bd99d4c059f60fb17c48d53166bb5c54a6339
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-08 14:07:49 +0700

    https://lqdoj.edu.vn/problem/cdl2p6

---
+++ cdl2p6.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    (n<18) ? cout<<"Ban chua den tuoi bau cu " : cout<<"Ban da du tuoi bau cu";
+}
