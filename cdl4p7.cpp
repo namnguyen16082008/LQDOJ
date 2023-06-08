8196d6a4663b1fa0fd97c69688335c7af0600d1e
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-08 13:58:58 +0700

    https://lqdoj.edu.vn/problem/cdl4p7

---
+++ cdl4p7.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++) cout<<"Hello world!"<<endl;
+}
