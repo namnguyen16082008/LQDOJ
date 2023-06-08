c471b1e0d17007c58e1a5c40dd8b546eea1f31dd
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-08 14:06:07 +0700

    https://lqdoj.edu.vn/problem/cdl1p13

---
+++ cdl1p13.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    cout<<"Moi hoc sinh duoc chia "<<b/a<<" qua man."<<endl<<"So man danh rieng cho cac em nu la "<<b%a<<".";
+}
