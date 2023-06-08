befe1a1e8ca11a5bc55488f387162111c108d282
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-08 13:46:48 +0700

    https://lqdoj.edu.vn/problem/cdl1p14

---
+++ cdl1p14.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    cout<<"So ban toi thieu can mua: "<<(a+1)/2+(b+1)/2+(c+1)/2;
+}
