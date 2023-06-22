ab698b49b4e09aa1d33d6a41d423d7857356a8e8
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-22 23:37:06 +0700

    https://lqdoj.edu.vn/problem/goala

---
+++ goala.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll t,n;
+void Solve(){
+    cin>>n;
+    cout<<n-1<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while (t--) Solve();
+}
