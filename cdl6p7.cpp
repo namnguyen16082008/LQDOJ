0d7993a6c29bef5c4a887680468135be7ade11f6
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-26 16:16:36 +0700

    https://lqdoj.edu.vn/problem/cdl6p7

---
+++ cdl6p7.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+double a,ans=0;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++){
+        cin>>a;
+        ans+=a;
+    }
+    cout<<fixed<<setprecision(2)<<ans/n;
+}
