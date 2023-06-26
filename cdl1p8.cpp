c436777df92f9c7af27bb634fa110bd34c6b771c
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-26 16:13:27 +0700

    https://lqdoj.edu.vn/problem/cdl1p8

---
+++ cdl1p8.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+
+ll a,b,u,v,x,y;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>u>>v>>x>>y;
+    cout<<"Tong so tien ve: "<<(a+u)*(x-y)+(b+v)*y<<" nghin dong.";
+}
