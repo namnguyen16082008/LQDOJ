29e23de9390da818dc3d25a439a16e676bb2ba61
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-27 17:30:27 +0700

    https://lqdoj.edu.vn/problem/cdl5p13

---
+++ cdl5p13.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    if (n==0) cout<<"zero";
+    else if (n==1) cout<<"one";
+    else if (n==2) cout<<"two";
+    else if (n==3) cout<<"three";
+    else if (n==4) cout<<"four";
+    else if (n==5) cout<<"five";
+    else if (n==6) cout<<"six";
+    else if (n==7) cout<<"seven";
+    else if (n==8) cout<<"eight";
+    else cout<<"nine";
+}
