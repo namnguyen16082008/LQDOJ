5125675d1d49fbdc7a02a5d2256c5a7189ec5e0a
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-27 17:50:18 +0700

    https://lqdoj.edu.vn/problem/cdl5p8

---
+++ cdl5p8.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+char k;
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>s;
+    cout<<"Ngay "<<s[0]<<s[1]<<" thang "<<s[3]<<s[4]<<" nam "<<s[6]<<s[7]<<s[8]<<s[9];
+}
